51b3440a82f0471b5cadf95b913d422a11b064ab
Alex <oleksiy.vt@gmail.com>
2019-02-20 10:24:35 +0200

    Settings bound for notifications about errors in app.

--- app/src/main/java/com/alexvt/integrity/settings/NotificationSettingsFragment.kt
+++ app/src/main/java/com/alexvt/integrity/settings/NotificationSettingsFragment.kt
@@ -7,11 +7,34 @@
 package com.alexvt.integrity.settings
 
 import android.os.Bundle
+import androidx.preference.CheckBoxPreference
+import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
+import androidx.preference.SwitchPreferenceCompat
 import com.alexvt.integrity.R
+import com.alexvt.integrity.core.IntegrityCore
 
 class NotificationSettingsFragment : PreferenceFragmentCompat() {
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.settings_notifications, rootKey)
+
+        bindShowErrors()
+    }
+
+    private fun bindShowErrors() {
+        val prefShowErrors: SwitchPreferenceCompat = findPreference("notifications_errors_enable")
+        updateShowErrors(prefShowErrors)
+        prefShowErrors.onPreferenceClickListener = Preference.OnPreferenceClickListener {
+            val showErrors = IntegrityCore.settingsRepository.get().notificationShowErrors
+            IntegrityCore.settingsRepository.set(context!!, IntegrityCore.settingsRepository.get()
+                    .copy(notificationShowErrors = !showErrors))
+            updateShowErrors(prefShowErrors)
+            IntegrityCore.notifyAboutUnreadErrors(context!!)
+            true
+        }
+    }
+
+    private fun updateShowErrors(prefShowErrors: SwitchPreferenceCompat) {
+        prefShowErrors.isChecked = IntegrityCore.settingsRepository.get().notificationShowErrors
     }
 }
